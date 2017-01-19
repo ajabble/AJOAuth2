fa9ba5bd4eb65ef24a4ff567701326d4ff54b26a
Ashish Jabble <ashish.jabble@nerdapplabs.com>
2017-01-19 12:02:54 +0530

    Initial Commit

---
+++ AJOAuth2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AJOAuth2
+//
+//  Created by Ashish Jabble on 19/01/17.
+//  Copyright © 2017 Ashish Jabble. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AJOAuth2/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AJOAuth2
+//
+//  Created by Ashish Jabble on 19/01/17.
+//  Copyright © 2017 Ashish Jabble. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
